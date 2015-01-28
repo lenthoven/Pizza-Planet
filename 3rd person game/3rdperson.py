232d8a0d8dc9cf58ad085034b81846b5e7c7e9f2
lenthoven <lenthoven1@gmail.com>
2015-01-27 18:54:29 -0800

    added character with positions
    added borders to the screen

--- 3rd person game/3rdperson.py
+++ 3rd person game/3rdperson.py
@@ -1,31 +1,48 @@
 import pygame
 import random
+pygame.init()
+screen = pygame.display.set_mode((480,360))
+
 up = False
 down = False
 right = False
 left = False
 class Player:
 	def __init__(self):
-		self.playerList = [pygame.image.load("images/cave.png")]
-		self.playerx = -600
-		self.playery = -450
+		self.playerList = [pygame.image.load("images/cave.png"), pygame.image.load("images/character.png").convert_alpha(), pygame.image.load("images/character2.png").convert_alpha(),pygame.image.load("images/character3.png").convert_alpha(),pygame.image.load("images/character4.png").convert_alpha()]
+		self.playerx = 0
+		self.playery = 0
+		self.characterx = 220
+		self.charactery = 150
 		self.move = 0
+		self.costume = 1
 	def checkOff(self):
-		self.move = 10
+		if self.playerx >= 0:
+			self.playerx = 0
+		if self.playerx <= -1920:
+			self.playerx = -1920
+		if self.playery >= 0:
+			self.playery = 0
+		if self.playery <= -1440:
+			self.playery = -1440
 	def draw(self,aScreen):
 		aScreen.blit(self.playerList[0],(self.playerx,self.playery))
+		aScreen.blit(self.playerList[self.costume],(self.characterx,self.charactery))
 	def left(self):
-		self.playerx += 20
+		self.playerx += .1
+		self.costume = 1
 	def right(self):
-		self.playerx -= 20
+		self.playerx -= .1
+		self.costume = 2
 	def up(self):
-		self.playery += 20
+		self.playery += .1
+		self.costume = 3
 	def down(self):
-		self.playery -= 20
-
+		self.playery -= .1
+		self.costume = 4
 		
 Player = Player()
-screen = pygame.display.set_mode((1200,900))
+screen = pygame.display.set_mode((480,360))
 running = True
 while running:
 	screen.fill((0,0,0))
@@ -60,6 +77,7 @@ while running:
 		Player.left()
 	if right:
 		Player.right()
+	Player.checkOff()
 	Player.draw(screen)
 	pygame.display.flip()
 pygame.quit()
